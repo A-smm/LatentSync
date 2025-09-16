9fd42853cbb0cee7981b83547d526f5342eb2ec3
Author Name <email@example.com>
2025-09-16 17:22:10 +0000

    Your commit message

--- setup_env.sh
+++ setup_env.sh
@@ -10,9 +10,6 @@ conda install -y -c conda-forge ffmpeg
 # Python dependencies
 pip install -r requirements.txt
 
-# OpenCV dependencies
-sudo apt -y install libgl1
-
 # Download the checkpoints required for inference from HuggingFace
 huggingface-cli download ByteDance/LatentSync-1.6 whisper/tiny.pt --local-dir checkpoints
 huggingface-cli download ByteDance/LatentSync-1.6 latentsync_unet.pt --local-dir checkpoints
